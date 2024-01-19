2f8b2a3c70eccd6e61c7d8438da1fbaadaec4f16
Sebastian08LR <156454864+Sebastian08LR@users.noreply.github.com>
2024-01-19 11:48:51 -0500

    Solucion ejercicio numero inverso

---
+++ inverse_num.py
@@ -0,0 +1,5 @@
+numero = int(input("Ingrese un entero: "))
+numero_str = str(numero)
+numero_inverso = numero_str[::-1]
+   
+print(f"El número con los dígitos en orden inverso es {numero_inverso}")
