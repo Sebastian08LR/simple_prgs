9ab4cb8280df0c724ce38e54361d4602e5426152
Sebastian08LR <156454864+Sebastian08LR@users.noreply.github.com>
2024-01-22 12:00:59 -0500

    Cambio de variables y correccíon de la solución

--- promedio.py
+++ promedio.py
@@ -1,10 +1,9 @@
-notes = list([])
-cant_notes = int(input("Ingrese la cantidad de notas que guardará: "))
-sum_notas = 0
-for nota in range(cant_notes):
-    average = sum_notas/cant_notes
-    notas = notes.append(float(input(f"Ingrese la nota numero {nota+1}: ")))
-    sum_notas += notas
+nota1 = int(input("Ingrese la primera nota: "))
+nota2 = int(input("Ingrese la segunda nota: "))
+nota3 = int(input("Ingrese la tercera nota: "))
+nota4 = int(input("Ingrese la cuarta nota: "))        
 
-print(f"\n\n\tEl promedio de las notas es {average}")
-        
+suma = nota1 + nota2 + nota3 + nota4
+average = suma / 4
+
+print(f"El promedio de las notas es {average}")
